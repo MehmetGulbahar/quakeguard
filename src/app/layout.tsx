5a80f4159c21b5fca620be6ac12658c0e92e7ee1
MehmetGulbahar <121450823+MehmetGulbahar@users.noreply.github.com>
2025-02-14 09:47:29 +0300

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
-import { ThemeProvider } from "@/components/theme-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 import "leaflet/dist/leaflet.css"
 import { PageTransition } from "@/components/page-transition"
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,13 +34,17 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
-      <body className={inter.className} suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-          suppressHydrationWarning
         >
           <QueryProvider>
             <Navbar />
